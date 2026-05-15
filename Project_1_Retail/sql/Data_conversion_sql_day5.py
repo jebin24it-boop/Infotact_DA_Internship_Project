3d11dd48e246faf18c5a5bc48f3338479476a010
Sauravgopinathek <sauravgopinathek@gmail.com>
2026-05-15 18:27:09 +0530

    Converted the clened dataset into database and table

---
+++ Project_1_Retail/sql/Data_conversion_sql_day5.py
@@ -0,0 +1,14 @@
+
+# Code for converting Excel csv file into a database and then creating a table
+import pandas as pd
+import sqlite3
+
+df = pd.read_csv("D:\\Saurav-Sql\\Infotact_DA_Internship_Project\\Project_1_Retail\\sql\\final_cleaned_retail_dataset.csv")
+
+# Database Name - Sql_Database
+conn = sqlite3.connect("Sql_Database.db")
+
+# Table Name - Retail_Dataset
+df.to_sql("Retail_Dataset", conn, if_exists="replace", index=False)
+conn.close()
+print("✅ Ready for SQL!")
